55f383c1e6c769248dee13878c50e01c148af617
Pratikshit26 <pratikshitpfd@gmail.com>
2021-05-31 13:21:50 +0530

    front -b

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import Splashscreen from "./components/splashscreen";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [splash, setSplash] = React.useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setSplash(false);
+    }, 2000);
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {splash === true ? <Splashscreen /> : <Text>fsdfds</Text>}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
